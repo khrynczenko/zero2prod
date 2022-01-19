3782060ff36e5e32204fa59117175aee61936728
Krzysztof Hrynczenko <jeniopy@gmail.com>
2022-01-19 18:40:06 +0100

    feat: add subscriptions endpoint and persistence

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,33 @@
+use actix_web::{web, HttpResponse, Responder};
+use sqlx::PgPool;
+
+use chrono::Utc;
+use uuid::Uuid;
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+pub async fn subscribe(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> impl Responder {
+    let query_result = sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(db_pool.get_ref())
+    .await;
+    match query_result {
+        Ok(_rows_affected) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
