4da4eb15807dd591011392bc5d18810dfe8f14fd
Krzysztof Hrynczenko <jeniopy@gmail.com>
2022-06-18 10:58:06 +0200

    refactor: use TcpListener instead of &str

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let address = "127.0.0.1:8000";
+    let tcp_listener = TcpListener::bind(address).expect("could not bind to an address");
+    run(tcp_listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,13 +1,18 @@
+use std::net::TcpListener;
+
 use reqwest::{Client, StatusCode};
+
+type BoundAddress = String;
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let bound_address = spawn_app();
+    let endpoint_address = format!("{}/health_check", bound_address);
 
     let client = Client::new();
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(endpoint_address)
         .send()
         .await
         .expect("Failed to execute the request");
@@ -15,7 +20,15 @@ async fn health_check_works() {
     assert!(response.status() == StatusCode::OK);
 }
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Could not run a server");
+fn spawn_app() -> BoundAddress {
+    let address = "127.0.0.1:0";
+    let tcp_listener = TcpListener::bind(address).expect("could not bind to an address");
+    let socket_address = tcp_listener
+        .local_addr()
+        .expect("could not get socket address");
+    let bound_address = socket_address.to_string();
+
+    let server = zero2prod::run(tcp_listener).expect("Could not run a server");
     tokio::spawn(server);
+    format!("http://{}", bound_address)
 }
